c35ac5527a97aebd6e58a81efeaa1af0b8c3ba51
Juan Diego Colmenares <cielocfd@gmail.com>
2023-02-26 22:36:25 -0800

    Fixed shapenet

--- PT_NIF/model/shape_net.py
+++ PT_NIF/model/shape_net.py
@@ -23,8 +23,8 @@ import torch.nn.functional as F
 def ShapeNet(x,params,N_l=4,N_s=2,act_fn=F.silu):
     w0 = torch.reshape(params[:,0:N_s],(-1,1,N_s))
     w1 = torch.reshape(params[:,N_s:((N_s+1)*N_s)],(-1,N_s,N_s))
-    w2 = torch.reshape(params[:,(2*N_s**2+N_s):(2*N_s**2+2*N_s)],(-1,N_s,N_s))
-    w3 = torch.reshape(params[:,(2*N_s**2+5*N_s):],(-1,N_s,1))
+    w2 = torch.reshape(params[:,(N_s**2+N_s):(2*N_s**2+N_s)],(-1,N_s,N_s))
+    w3 = torch.reshape(params[:,(2*N_s**2+N_s):(2*N_s**2+2*N_s)],(-1,N_s,1))
     b0   = torch.reshape(params[:,(2*N_s**2+2*N_s):(2*N_s**2+3*N_s)],(-1, N_s))
     b1   = torch.reshape(params[:,(2*N_s**2+3*N_s):(2*N_s**2+4*N_s)],(-1, N_s))
     b2   = torch.reshape(params[:,(2*N_s**2+4*N_s):(2*N_s**2+5*N_s)],(-1, N_s))
@@ -34,4 +34,4 @@ def ShapeNet(x,params,N_l=4,N_s=2,act_fn=F.silu):
     u = act_fn(torch.einsum('ij,ijk->ik',u,w1) + b1) + u
     u = act_fn(torch.einsum('ij,ijk->ik',u,w2) + b2) + u
     u = torch.einsum('ij,ijk->ik',u,w3) + b3
-    return x
+    return u
